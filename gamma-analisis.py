878453685ac5473f32b6e4b631b726e9b3dce799
DavidBamba <dbambague@unal.edu.co>
2024-03-07 18:44:59 -0500

    comit

--- gamma-analisis.py
+++ gamma-analisis.py
@@ -6,7 +6,7 @@ from scipy.integrate import simps
 import re  
 
 # Path to the VTK files
-directory = '/home/yo/Documents/Athena-Cversion/bin'
+directory = '/home/yo/Documents/Athena-Cversion/results/results-ATHV3/output_combined_G'
 vtk_files = glob.glob(directory + '/combined_*.vtk')
 
 # Function to extract the timestep from the filename
@@ -14,7 +14,7 @@ def filename_to_time(filename):
     match = re.search(r'combined_(\d+).vtk', filename)
     if match:
         # Convert the timestep number to an integer, then to real time
-        time_step = 0.02
+        time_step = 0.01
         return int(match.group(1)) * time_step
     else:
         return None
@@ -27,7 +27,7 @@ Lz = 0.2
 
 # Lists to store time and total energy for each timestep
 times = []  
-total_energy_per_timestep = []
+Gamma_timestep = []
 
 # Process each file
 for vtk_file in sorted(vtk_files, key=filename_to_time):
@@ -37,24 +37,23 @@ for vtk_file in sorted(vtk_files, key=filename_to_time):
 
         # Read the mesh from the VTK file
         mesh = pv.read(vtk_file)
-        U_b = mesh['G']  # Assuming 'G' is the quantity you want to integrate
-        U_b_2d = U_b.reshape(nz, ny)
+        G = mesh['G']  # Assuming 'G' is the quantity you want to integrate
+        G_2d = G.reshape(nz, ny)
 
         # Integrate b^2 over the domain
         dy = Ly / ny
         dz = Lz / nz
-        total_energy = np.trapz([np.trapz(U_b_2d[ii, :], dx=dy) for ii in range(nz)], dx=dz)
-        total_energy_per_timestep.append(total_energy)
+        Gamma = np.trapz([np.trapz(G_2d[ii, :], dx=dy) for ii in range(nz)], dx=dz)
+        Gamma_timestep.append(Gamma)
 
 # Calculate the average energy over all timesteps
-average_energy = np.mean(total_energy_per_timestep)
+average_energy = np.mean(Gamma_timestep) 
 
 # Plotting the result
 plt.figure(figsize=(10, 6))
-plt.plot(times, total_energy_per_timestep, label='Energy per Timestep')
+plt.plot(times, Gamma_timestep)
 plt.hlines(average_energy, xmin=min(times), xmax=max(times), colors='r', linestyles='dashed', label='Average Energy')
 plt.xlabel('Time')
-plt.ylabel('Total Magnetic Energy')
-plt.title('Total Magnetic Energy Over Time')
-plt.legend()
+plt.ylabel('Gamma')
+plt.yscale('log')
 plt.show()
