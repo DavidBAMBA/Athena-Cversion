523f814e4af47da92b8daa2fbdf2e32c7af9bbf2
DavidBamba <dbambague@unal.edu.co>
2024-03-07 11:45:23 -0500

    analisis

--- Reconection-rate-analisis.py
+++ Reconection-rate-analisis.py
@@ -7,7 +7,7 @@ import re
 import pandas as pd
 
 # Path to the VTK files
-directory = '/home/yo/Documents/Athena-Cversion/bin/output_combined'
+directory = '/home/yo/Documents/Athena-Cversion/results/results-ATHV2/output_combined_Bsqr'
 vtk_files = glob.glob(directory + '/combined_*.vtk')
 
 # Function to extract the timestep from the filename
@@ -22,7 +22,7 @@ def filename_to_time(filename):
 
 # Parameters
 ny = 512
-nz = 918
+nz = 1024
 Ly = 0.1
 Lz = 0.2
 
@@ -38,7 +38,7 @@ for vtk_file in sorted(vtk_files, key=filename_to_time):
 
         # Read the mesh from the VTK file
         mesh = pv.read(vtk_file)
-        print(mesh.point_data.keys())
+        #print(mesh.point_data.keys())
 
         U_b = mesh['Bsqr'] 
         U_b_2d = U_b.reshape(nz, ny)
@@ -56,19 +56,21 @@ for vtk_file in sorted(vtk_files, key=filename_to_time):
 
 # Calculate the temporal derivative of the dissipated magnetic energy
 # Use forward finite difference for the derivative
-energy_dissipation_rate = np.diff(total_magnetic_energy) / np.diff(times)
+energy_dissipation_rate = np.diff(total_magnetic_energy) / np.diff(times) 
 
 # Plot the magnetic energy dissipation rate
 plt.figure(figsize=(10, 5))
-plt.plot(times[:-1], -energy_dissipation_rate, label='Magnetic Energy Dissipation Rate')
+plt.plot(times[:-1], -energy_dissipation_rate/10.0, label='Magnetic Energy Dissipation Rate')
 plt.xlabel('Time')
+plt.yscale('log')
 plt.ylabel('Magnetic Energy Dissipation Rate')
 plt.legend()
 plt.show()
 
-# Optionally, create a DataFrame and save it to a CSV file
+""" # Optionally, create a DataFrame and save it to a CSV file
 df = pd.DataFrame({
     'Time': times[:-1],
     'Dissipation Rate': -energy_dissipation_rate
 })
 df.to_csv('/home/yo/Documents/output_combined/magnetic_energy_dissipation_rate.csv', index=False)
+ """
