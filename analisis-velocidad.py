9106714c7cd2cbc49710a15416ee2b228734f700
DavidBamba <dbambague@unal.edu.co>
2024-03-07 15:25:07 -0500

    comit

--- analisis-velocidad.py
+++ analisis-velocidad.py
@@ -4,26 +4,23 @@ import matplotlib.pyplot as plt
 import glob
 
 # Reemplace esto con la ruta a su archivo VTK
-vtk_file_path = '/home/yo/Documents/Athena-Cversion/results/results-ATHV2/output_combined_vz/combined_0002.vtk'  
+vtk_file_path = '/home/yo/Documents/Athena-Cversion/results/results-ATHV2/output_combined_vz/combined_0001.vtk'  
 
 
 # Leer el archivo VTK
 mesh = pv.read(vtk_file_path)
 
-print("Point Data Keys:")
-print(mesh.point_data.keys())
-
-# Imprimir las keys de los datos de celdas
-print("\nCell Data Keys:")
-print(mesh.cell_data.keys())
 
 # Extraer la velocidad en la dirección z (V3)
 velocity_z = mesh['vz']
+velocity_z_2d = velocity_z.reshape(1024, 512)
+max_vel = np.max(velocity_z_2d, axis=0)
 
 # Calcular la velocidad máxima y mínima en z
 max_vz = np.max(velocity_z)
 min_vz = np.min(velocity_z)
-
+print(velocity_z.shape)
+y = np.linspace(-0.05, 0.05, 512)
 # Crear una gráfica que muestre la velocidad máxima y mínima
 plt.figure(figsize=(8, 6))
 plt.plot(velocity_z, label='Vz')
